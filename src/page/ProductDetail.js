54ed3b2295c995d289e16917e7852bd65ae9f89d
hongyungeun <78795348+hongyungeun@users.noreply.github.com>
2022-07-25 03:30:00 +0900

    urlChange

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -6,7 +6,7 @@ export default function ProductDetail() {
   let {id} = useParams()
   const [product,setProduct]= useState(null)
   const getProductDetail = async()=>{
-    let url = `http://localhost:5000/products/${id}`
+    let url = `https://my-json-server.typicode.com/hongyungeun/react-router-practice/products/${id}`
     let response = await fetch(url)
     let data = await response.json()
     setProduct(data)
